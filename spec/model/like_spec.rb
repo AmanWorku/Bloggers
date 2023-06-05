7090027783835d5cec6c4d3626f781fcb61bba70
AmanWorku <amexworku2014@gmail.com>
2023-06-05 16:03:30 +0300

    Add test for the like model

---
+++ spec/model/like_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe Like, type: :model do
+  describe 'Validations' do
+    user = User.create(name: 'Aman', photo: 'Image goes here', bio: 'Hi There.', posts_counter: 0)
+    post = Post.create(title: 'First Post', text: 'This is my first post', author_id: user.id, comments_counter: 0,
+                       likes_counter: 0)
+
+    subject { described_class.create(post: post, author: user) }
+
+    it 'must contain user id' do
+      subject.author_id = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'must contain post id' do
+      subject.post_id = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'increments the likes counter of the associated post' do
+      expect(subject.post.likes_counter).to eq(post.likes_counter)
+    end
+  end
+end
