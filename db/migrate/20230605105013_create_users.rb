201e53c621a824132428b05aba964612f78f0778
AmanWorku <amexworku2014@gmail.com>
2023-06-05 13:55:52 +0300

    Create table and update migration files

--- db/migrate/20230605105013_create_users.rb
+++ db/migrate/20230605105013_create_users.rb
@@ -1,4 +1,4 @@
-class CreateUsers < ActiveRecord::Migration[7.0]
+class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       t.string :name
@@ -8,5 +8,7 @@ class CreateUsers < ActiveRecord::Migration[7.0]
 
       t.timestamps
     end
+
+    add_index :users, :name
   end
 end
