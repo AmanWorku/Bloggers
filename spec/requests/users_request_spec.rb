415e0fd090261bfd569286b787389871204e18af
AmanWorku <amexworku2014@gmail.com>
2023-06-12 11:39:09 +0300

    Fix linter error

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -1,26 +1,25 @@
 require 'rails_helper'
 
- # we group together our users tests
- RSpec.describe '/users', type: :request do
-   context 'GET /index' do
-     it 'renders a successful response' do
-       get users_url
-       expect(response).to be_successful
-     end
+# we group together our users tests
+RSpec.describe '/users', type: :request do
+  context 'GET /index' do
+    it 'renders a successful response' do
+      get users_url
+      expect(response).to be_successful
+    end
 
-     it 'renders the correct template' do
-       get users_url
-       expect(response).to render_template('index')
-     end
+    it 'renders the correct template' do
+      get users_url
+      expect(response).to render_template('index')
+    end
 
-     it 'includes correct placeholder text' do
-       get users_url
-       expect(response.body).to include('<h1>Here is a list of all users</h1>')
-     end
-   end
+    it 'includes correct placeholder text' do
+      get users_url
+      expect(response.body).to include('<h1>Here is a list of all users</h1>')
+    end
+  end
 
-
-   context 'GET /show' do
+  context 'GET /show' do
     it 'renders a successful response' do
       get users_url(5)
       expect(response).to be_successful
@@ -36,4 +35,4 @@ require 'rails_helper'
       expect(response.body).to include('<h1>Here is the user for a given ID</h1>')
     end
   end
- end
+end
