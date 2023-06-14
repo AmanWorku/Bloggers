fc5bc2368792fa7b3ccc7cfe95f9fb2298af3201
AmanWorku <amexworku2014@gmail.com>
2023-06-14 08:47:02 +0300

    Add user data to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+first_user = User.create(name: 'Aman', photo: 'https://avatars.githubusercontent.com/u/80094659?v=4', bio: 'Learning More.')
+ second_user = User.create(name: 'Bese', photo: 'https://avatars.githubusercontent.com/u/23375153?v=4', bio: 'Living my dream.')
+ third_user = User.create(name: 'Yidne', photo: 'https://avatars.githubusercontent.com/u/104775335?v=4', bio: 'See you in the next round.')
